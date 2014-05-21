6d23ae4f5543782a587fc4c39dc3ed96dd23dadc
Ruoshi.Ling <fntsrlike@gmail.com>
2014-05-21 17:27:05 +0800

    Rebuild version first commit

---
+++ js/member.js
@@ -0,0 +1,147 @@
+var ns_member;
+
+ns_member = [
+
+var member = [
+  {
+    id: 'sntc06',
+    nick: '雪人'
+    title: '融化中請稍候',
+    avatar: 'http://farm3.staticflickr.com/2875/9069037713_1752f5daeb.jpg',
+    content: '神秘的老機團副團長，曾多次協助電資島上的事務。對於電腦硬體有相當強的執著（?）。技能：雪人的祝福，（附加效果 智力-5 魅力-5 大叔+10）。'
+  },
+  {
+    id: 'fntsrlike',
+    nick: '團長'
+    title: '我是幼稚鬼',
+    avatar: '',
+    content: '幼稚鬼團團長，一個莫名其妙的老人。對長直髮正妹有著強大的癡迷是其致命的弱點。'
+  },
+  {
+    id: 'cyrandy',
+    title: '',
+    avatar: '',
+    content: ''
+  },
+  {
+    id: '',
+    title: '',
+    avatar: '',
+    content: ''
+  },
+  {
+    id: '',
+    title: '',
+    avatar: '',
+    content: ''
+  },
+  {
+    id: '',
+    title: '',
+    avatar: '',
+    content: ''
+  },
+  {
+    id: '',
+    title: '',
+    avatar: '',
+    content: ''
+  },
+  {
+    id: '',
+    title: '',
+    avatar: '',
+    content: ''
+  },
+  {
+    id: '',
+    title: '',
+    avatar: '',
+    content: ''
+  },
+  {
+    id: '',
+    title: '',
+    avatar: '',
+    content: ''
+  },
+  {
+    id: '',
+    title: '',
+    avatar: '',
+    content: ''
+  },
+  {
+    id: '',
+    title: '',
+    avatar: '',
+    content: ''
+  },
+  {
+    id: '',
+    title: '',
+    avatar: '',
+    content: ''
+  },
+  {
+    id: '',
+    title: '',
+    avatar: '',
+    content: ''
+  },
+  {
+    id: '',
+    title: '',
+    avatar: '',
+    content: ''
+  },
+  {
+    id: '',
+    title: '',
+    avatar: '',
+    content: ''
+  },
+  {
+    id: '',
+    title: '',
+    avatar: '',
+    content: ''
+  },
+  {
+    id: '',
+    title: '',
+    avatar: '',
+    content: ''
+  },
+  {
+    id: '',
+    title: '',
+    avatar: '',
+    content: ''
+  },
+  {
+    id: '',
+    title: '',
+    avatar: '',
+    content: ''
+  },
+  {
+    id: '',
+    title: '',
+    avatar: '',
+    content: ''
+  },
+  {
+    id: '',
+    title: '',
+    avatar: '',
+    content: ''
+  },
+  {
+    id: '',
+    title: '',
+    avatar: '',
+    content: ''
+  },
+];
+]
